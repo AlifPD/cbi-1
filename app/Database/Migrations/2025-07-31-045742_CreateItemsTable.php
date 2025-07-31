955994573563096fe4db43e369886964001a1ea1
Muhammad Alif Putra Dafi <muhammadalifputradafi@gmail.com>
2025-07-31 12:06:48 +0700

    Create migration and model, auto generate from php spark

---
+++ app/Database/Migrations/2025-07-31-045742_CreateItemsTable.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateItemsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'quantity' => [
+                'type' => 'INT',
+            ],
+            'category_id' => [
+                'type'       => 'INT',
+                'unsigned'   => true,
+                'null'       => true,
+            ],
+            'created_at' => [
+                'type'    => 'DATETIME',
+                'null'    => true,
+            ],
+            'updated_at' => [
+                'type'    => 'DATETIME',
+                'null'    => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('category_id', 'categories', 'id', 'CASCADE', 'SET NULL');
+        $this->forge->createTable('items');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('items');
+    }
+}
