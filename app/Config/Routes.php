b8bdd244f76290ce4693cab91d22e658f497ee6b
Muhammad Alif Putra Dafi <muhammadalifputradafi@gmail.com>
2025-07-31 12:15:41 +0700

    Create controller dan route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,12 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+$routes->group('items', function ($routes) {
+    $routes->get('/', 'ItemController::index');
+    $routes->get('fetch', 'ItemController::fetch');
+    $routes->get('categories', 'ItemController::getCategories');
+    $routes->post('create', 'ItemController::create');
+    $routes->post('update/(:num)', 'ItemController::update/$1');
+    $routes->post('delete/(:num)', 'ItemController::delete/$1');
+});
